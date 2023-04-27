7ed2172441af02e9c77db2e1cbdaf3630c57a063
Mickaël AUGER <auger.mickael37@gmail.com>
2023-04-27 15:12:44 +0200

    Mes premières pages avec Symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'name' => 'home',
+        ]);
+    }
+
+    #[Route('/contact', name: 'home_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
+    }
+}
