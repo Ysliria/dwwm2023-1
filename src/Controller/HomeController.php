d010a4d86298d4f1245d98b095f38100d47ddf62
Mickaël AUGER <auger.mickael37@gmail.com>
2023-04-28 16:54:27 +0200

    Mise en place des cards réutilisables

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig' ,[
+            'formations' => $formationRepository->findBy([], ['startedAt' => 'asc'], '3')
+        ]);
     }
 
     #[Route('/contact', name: 'home_contact')]
