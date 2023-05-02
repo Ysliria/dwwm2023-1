25b0555c0016ca6aacf9d5943d89f69301643add
Mickaël AUGER <auger.mickael37@gmail.com>
2023-05-02 14:49:27 +0200

    Ajout formulaires de création et de mise à jour

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
+use App\Form\FormationType;
 use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/formation', name: 'formation_')]
 class FormationController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(FormationRepository $formationRepository): Response
     {
         return $this->render('formation/index.html.twig', [
@@ -19,11 +21,50 @@ class FormationController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'show')]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Formation $formation): Response
     {
         return $this->render('formation/show.html.twig', [
             'formation' => $formation
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'], priority: 2)]
+    public function new(Request $request, FormationRepository $formationRepository): Response
+    {
+        $formation    = new Formation();
+        $formationAdd = $this->createForm(FormationType::class, $formation);
+
+        $formationAdd->handleRequest($request);
+
+        if ($formationAdd->isSubmitted() && $formationAdd->isValid()) {
+            $formationRepository->save($formation, true);
+            $this->addFlash('success', 'La formation " ' . $formation->getNom() . ' " a bien été créée');
+
+            return $this->redirectToRoute('formation_index', [], Response::HTTP_CREATED);
+        }
+
+        return $this->render('formation/new.html.twig', [
+            'formation_add' => $formationAdd->createView()
+        ]);
+    }
+
+    #[Route('/{id}/update', name: 'update', methods: ['GET', 'POST'])]
+    public function update(Request $request, Formation $formation, FormationRepository $formationRepository): Response
+    {
+        $formationUpdate = $this->createForm(FormationType::class, $formation);
+
+        $formationUpdate->handleRequest($request);
+
+        if ($formationUpdate->isSubmitted() && $formationUpdate->isValid()) {
+            $formationRepository->save($formation, true);
+            $this->addFlash('success', 'La formation " ' . $formation->getNom() . ' " a bien été mise à jour');
+
+            return $this->redirectToRoute('formation_show', ['id' => $formation->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('formation/update.html.twig', [
+            'formation_update' => $formationUpdate->createView()
         ]);
     }
 }
---
+++ src/Form/FormationType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la formation',
+                'attr'  => [
+                    'placeholder' => 'Concepteur Développeur d\'Application'
+                ]
+            ])
+            ->add('code', TextType::class, [
+                'label' => 'Code de la formation',
+                'attr'  => [
+                    'placeholder' => 'CDA'
+                ]
+            ])
+            ->add('startedAt', DateTimeType::class, [
+                'label' => 'Date de début de la formation',
+                'input' => 'datetime_immutable'
+            ])
+            ->add('finishedAt', DateTimeType::class, [
+                'label' => 'Date de fin de la formation',
+                'input' => 'datetime_immutable',
+                'help'  => 'La date de fin doit être postérieur à la date de début'
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Lieu de la formation',
+                'attr'  => [
+                    'placeholder' => 'Tours'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => Formation::class,
+            ]
+        );
+    }
+}
