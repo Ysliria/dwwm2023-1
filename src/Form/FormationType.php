2e096491830ae1c2ade116634928bf6f2c397d0e
Mickaël AUGER <auger.mickael37@gmail.com>
2023-05-04 10:06:11 +0200

    Ajout authentification

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
